d3d0b2cc2948d5932d82de624c55f7b2f32d2873
Esmeralda Gallardo <gallardo.esmeralda05@gmail.com>
2022-10-10 16:31:43 -0300

    Temporary change banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/images/a.png";
+import headerImg from "../assets/images/header-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
